c80418a7fb72677edd96cbcd2db7f6adb66abfc5
Надия Миляева <@corp.itstep>
2023-07-19 21:52:29 +0500

    second commint

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import requests
 
-url = "https://google.com"
-a = requests.get(url)
-a.raise_for_status()
-print(a.text)
+# url = "https://jsonplaceholder.typicode.com/todos"
+# a = requests.get(url)
+# a.raise_for_status()
+#
+# txt = "myfile.doc"
+# with open (txt, 'wb') as file:
+#     file.write(a.content)
+
+# a = requests.put('https://httpbin.org/put'
+#                  , data={'key', 'value'})
+# a.raise_for_status()
+# txt = "myfile1.doc"
+# with open (txt, 'wb') as file:
+#     file.write(a.content)
+
+
+# a = requests.delete('https://httpbin.org/delete')
+# a.raise_for_status()
+# txt = "myfile1.doc"
+# with open (txt, 'wb') as file:
+#     file.write(a.content)
+
+
+# a = requests.head('https://httpbin.org/get')
+# a.raise_for_status()
+# print(a)
+
+
+# a = requests.options('https://httpbin.org/get')
+# a.raise_for_status()
+# txt = "myfile1.doc"
+# with open (txt, 'wb') as file:
+#     file.write(a.content)
+# print(a)
